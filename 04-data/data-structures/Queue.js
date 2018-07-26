0cfa4e7d303650833a0d35193dd8d22c1bb14e76
Isaac Ramirez <iramirezc@live.com.mx>
2018-07-26 17:44:01 -0500

    implement data structure Queue

---
+++ 04-data/data-structures/Queue.js
@@ -0,0 +1,157 @@
+const defineProperty = Object.defineProperty;
+
+/**
+ * Node of a Queue
+ * @class
+ */
+const Node = (function () {
+  /**
+   * Initializes a new instance of a Node
+   * @constructs Node
+   * @param {*} value The holding value
+   * @param {Node|null} next The next node in the Queue
+   */
+  function Node(value, next = null) {
+    /**
+     * Node's value
+     * @name Node#_value
+     * @type {*}
+     */
+    this._value;
+
+    defineProperty(this, '_value', {
+      get() { return value; } /* static value */
+    });
+
+    /**
+     * Pointer to the next Node
+     * @name Node#_next
+     * @type {Node}
+     */
+    this._next;
+
+    defineProperty(this, '_next', {
+      writable: true, /* queues should be able to update next node */
+      value: next
+    });
+
+    return this;
+  }
+
+  /**
+   * Returns the value as string
+   * @function Node#toString
+   */
+  Node.prototype.toString = function () {
+    return String(this._value);
+  };
+
+  return Node;
+})();
+
+// ==================================================
+
+/**
+ * Queue
+ * @class
+ */
+const Queue = (function () {
+  /**
+   * Initializes a new instance of a Queue
+   * @constructs Queue
+   */
+  function Queue() {
+    /**
+     * First Node in the Queue
+     * @name Queue#_first
+     * @type {Node|null}
+     */
+    this._first;
+
+    defineProperty(this, '_first', {
+      writable: true,
+      value: null /* default */
+    });
+
+    return this;
+  }
+
+  /**
+   * Checks if the queue is empty.
+   * @function Queue#isEmpty
+   */
+  Queue.prototype.isEmpty = function () {
+    return this._first === null;
+  };
+
+  /**
+   * Adds a new item at the end of the queue.
+   * @function Queue#enqueue
+   * @param {*} value Value to be enqueued
+   */
+  Queue.prototype.enqueue = function (value) {
+    const node = new Node(value, null);
+
+    if (this.isEmpty()) {
+      this._first = node;
+    } else {
+      let lastNode = this._first;
+
+      while (lastNode._next !== null) {
+        lastNode = lastNode._next;
+      }
+
+      lastNode._next = node;
+    }
+
+    return this;
+  };
+
+  /**
+   * Removes and returns the first item in the queue. 
+   * @function Queue#dequeue
+   */
+  Queue.prototype.dequeue = function () {
+    if (this.isEmpty()) {
+      throw new Error('Queue is empty.');
+    }
+
+    const currentFirst = this._first;
+
+    if (currentFirst._next) {
+      this._first = currentFirst._next;
+    } else {
+      this._first = null;
+    }
+
+    return currentFirst._value;
+  };
+
+  /**
+   * Returns the queue as string
+   * @function Queue#toString
+   */
+  Queue.prototype.toString = function () {
+    if (this.isEmpty()) {
+      return '[Empty Queue]'
+    }
+
+    let queueAsString = '';
+    let currentNode = this._first;
+
+    while (currentNode) {
+      queueAsString += String(currentNode);
+      currentNode = currentNode._next;
+
+      if (currentNode) {
+        queueAsString += ',';
+      }
+    }
+
+    return queueAsString;
+  };
+
+  return Queue;
+})();
+
+module.exports = Queue;
---
+++ 04-data/data-structures/Queue.spec.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+
+const Queue = require('./Queue');
+
+describe('Data Structure - Queue', function () {
+  let queue;
+
+  beforeEach(function () {
+    queue = new Queue();
+  });
+
+  it('should be intialized correctly', function () {
+    assert.ok(queue, 'queue is not intialized');
+  });
+
+  it('should start empty', function () {
+    assert.equal(queue.isEmpty(), true, 'queue did not start empty');
+  });
+
+  it('should throw an exception if trying to dequeue on an empty queue', function () {
+    assert.throws(() => { queue.dequeue() }, /^Error: Queue is empty.$/, 'queue is not throwing an Error');
+  });
+
+  it('should enqueue and dequeue the first item correctly', function () {
+    const value = 1;
+
+    queue.enqueue(value);
+
+    assert.equal(queue.dequeue(), value, 'incorrect item 1');
+    assert.equal(queue.isEmpty(), true, 'queue should be empty');
+  });
+
+  it('should enqueue and dequeue multiple items correctly', function () {
+    const data = [1, 2, 3, 4, 5, false, {}, [], 'hello'];
+
+    data.forEach(d => queue.enqueue(d));
+
+    const retrievedData = [];
+
+    while (!queue.isEmpty()) {
+      retrievedData.push(queue.dequeue());
+    }
+
+    assert.deepEqual(retrievedData, data, 'incorrect retrieved data');
+    assert.equal(queue.isEmpty(), true, 'queue should be empty');
+  });
+
+  it('should return the queue as string', function () {
+    const data = [1, 2, 3, 4, 5, false, {}, [], 'hello'];
+
+    assert.equal(queue.toString(), '[Empty Queue]', 'incorrect empty queue as string');
+
+    data.forEach(d => queue.enqueue(d));
+
+    assert.equal(queue.toString(), data.join(','), 'incorrect queue as string');
+  });
+});

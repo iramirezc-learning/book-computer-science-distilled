490b79de10c19be35f89deb82d62d68d995104e6
Isaac Ramirez <iramirezc@live.com.mx>
2018-08-09 03:01:02 -0500

    implemented reusable Node class for ADTs

---
+++ 04-data/data-structures/Node.js
@@ -0,0 +1,153 @@
+const defineProperty = Object.defineProperty;
+
+/**
+ * Validates if a node is an instance of a Node or null.
+ * @param {(Node|null)} node The next pointer value.
+ */
+function isNodeOrNull(node) {
+  if (node !== null && !(node instanceof Node)) {
+    throw new Error(`"next" should be an instance of Node or null.`);
+  }
+}
+
+/**
+ * Node Abstract Data Type
+ * @class Node
+ */
+const Node = (function () {
+  /**
+   * Initializes a new instance of a Node
+   * @constructs Node
+   * @param {*} value The holding value
+   */
+  function Node(value, priority = 0) {
+    if (!Number.isInteger(priority)) {
+      throw new Error('"priority" should be an integer.');
+    }
+
+    /**
+     * Pointer to the next Node
+     * @type {(Node|null)}
+     * @private
+     * @default null
+     */
+    let _next = null;
+
+    /**
+     * Holding value
+     * @name Node#_value
+     * @type {*}
+     * @readonly
+     */
+    this._value;
+
+    defineProperty(this, '_value', {
+      get() { return value; }
+    });
+
+    /**
+     * Holding value
+     * @name Node#value
+     * @type {*}
+     * @readonly
+     */
+    this.value;
+
+    defineProperty(this, 'value', {
+      get() { return value; }
+    });
+
+    /**
+     * Pointer to the next Node
+     * @name Node#_next
+     * @type {(Node|null)}
+     * @readonly
+     * @default null
+     */
+    this._next;
+
+    defineProperty(this, '_next', {
+      get() { return _next; }
+    });
+
+    /**
+     * Pointer to the next Node
+     * @name Node#next
+     * @type {(Node|null)}
+     * @default null
+     */
+    this.next;
+
+    defineProperty(this, 'next', {
+      set(newNext) { isNodeOrNull(newNext); _next = newNext; },
+      get() { return _next; }
+    });
+
+    /**
+     * Priority of the Node for PriorityQueues
+     * @name Node#_priority
+     * @type {int}
+     * @readonly
+     * @default 0
+     */
+    this._priority;
+
+    defineProperty(this, '_priority', {
+      get() { return priority; }
+    });
+
+    /**
+     * Priority of the Node for PriorityQueues
+     * @name Node#priority
+     * @type {int}
+     * @readonly
+     * @default 0
+     */
+    this.priority;
+
+    defineProperty(this, 'priority', {
+      get() { return priority; }
+    });
+
+    return this;
+  }
+
+  /**
+   * Tells if the current node has a next node
+   * @function Node#hasNext
+   */
+  Node.prototype.hasNext = function () {
+    return this.next !== null;
+  };
+
+  /**
+   * Updates the pointer to the next node
+   * @function Node#setNext
+   * @param {(Node|null)} newNext 
+   */
+  Node.prototype.setNext = function(newNext) {
+    this.next = newNext;
+
+    return this;
+  };
+
+  /**
+   * Returns the next node.
+   * @function Node#getNext
+   */
+  Node.prototype.getNext = function() {
+    return this.next;
+  };
+
+  /**
+   * Returns the value as string
+   * @function Node#toString
+   */
+  Node.prototype.toString = function () {
+    return String(this._value);
+  };
+
+  return Node;
+})();
+
+module.exports = Node;
---
+++ 04-data/data-structures/Node.spec.js
@@ -0,0 +1,77 @@
+const assert = require('assert');
+
+const Node = require('./Node');
+
+describe('Data Structure - Node', function() {
+  const intialValue = 'Initial Value';
+  /** @type {Node} */
+  let node;
+
+  beforeEach(function() {
+    node = new Node(intialValue);
+  });
+
+  it('should be initialized correctly', function() {
+    assert.ok(node, 'node is not initialized');
+    assert.equal(node._value, intialValue, '_value is incorrect');
+    assert.equal(node.value, intialValue, 'value is incorrect');
+    assert.equal(node._next, null, 'invalid _next pointer');
+    assert.equal(node.next, null, 'invalid next pointer');
+    assert.equal(node._priority, 0, 'invalid _priority');
+    assert.equal(node.priority, 0, 'invalid priority');
+  });
+
+  it('should not allow to change the readonly value', function() {
+    const newValue = 'some other value';
+
+    node._value = newValue;
+    assert.equal(node._value, intialValue, '_value has changed');
+    node.value = newValue;
+    assert.equal(node.value, intialValue, 'value has changed');
+  });
+
+  it('should throw an exception when initializing with an invalid priority', function() {
+    const invalidPriority = '1';
+    assert.throws(() => { new Node(intialValue, invalidPriority) }, /^Error: "priority" should be an integer\.$/, 'Node constructor is not throwing an Error');
+  });
+
+  it('should update the next node correctly to null', function() {
+    const newNext = null;
+
+    node._next = newNext;
+    assert.equal(node._next, node.getNext(), '_next should not have changed');
+    node.next = newNext;
+    assert.equal(node.next, newNext, 'invalid next');
+    node.setNext(newNext);
+    assert.equal(node.getNext(), newNext, 'invalid getNext');
+  });
+
+  it('should update the next node correctly to another node', function() {
+    const newNext = new Node(true);
+
+    node._next = newNext;
+    assert.equal(node._next, node.getNext(), '_next should not have changed');
+    node.next = newNext;
+    assert.equal(node.next, newNext, 'invalid next');
+    node.setNext(newNext);
+    assert.equal(node.getNext(), newNext, 'invalid getNext');
+  });
+
+  it('should throw an exception when new next is not an instance of Node or null', function() {
+    assert.throws(() => { node.next = undefined }, /^Error: "next" should be an instance of Node or null\.$/, 'next is not throwing an Error');
+    assert.throws(() => { node.setNext(undefined) }, /^Error: "next" should be an instance of Node or null\.$/, 'setNext is not throwing an Error');
+  });
+
+  it('should return false when there is no next node', function() {
+    assert.equal(node.hasNext(), false, 'invalid hasNext');
+  });
+
+  it('should return true when there is a next node', function() {
+    node.setNext(new Node('new value'));
+    assert.equal(node.hasNext(), true, 'invalid hasNext');
+  });
+
+  it('should return the node as string', function() {
+    assert.equal(node.toString(), String(intialValue), 'invalid node as string');
+  });
+});
